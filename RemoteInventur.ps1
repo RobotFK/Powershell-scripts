c5f244b85dca57df1f94a8d7c6ecd526d85b0c52
RobotFK <31999284+RobotFK@users.noreply.github.com>
2021-10-15 08:23:57 +0200

    WIP

--- RemoteInventur.ps1
+++ RemoteInventur.ps1
@@ -1,4 +1,5 @@
 ﻿#By Fabian Keusen under  GPL-3.0 License 
+#V0.2 Added more Stuff, mostly Framwork
 
 class pc{#What we take from Each user
         [String]$Seriennummer
@@ -21,40 +22,57 @@ ForEach($PC in $verschiedenePCNamen){
 }
 #>
 
+Write-Host "Warning: Execution requires local admin rights on queried devices" 
+
+$Singelconversion = -not( Test-Path HardwareInventory.csv) # If we are not Sharing a Folder with a Inventorylist we require the user to give a Indiviual Workstation
+if($Singelconversion){
+    $PClist = Read-Host "No HardwareInventory detected.`nEnter Target Workstation:"
+}else{
+
+}
 #Import goes towards $PClist
-$Monitorsarray = Get-WmiObject -Property SerialNumberID -Namespace "root/WMI" WmiMonitorID |Select-Object  SerialNumberID | %{[char[]]($_.SerialNumberID)};
-
-if([int]$Monitorsarray[1] -ne 0){
-$Monitorsarray[0..15]| Foreach-Object{ $PC.Monitor1 += $_;}}
-
-if([int]$Monitorsarray[17] -ne 0){
-$Monitorsarray[16..31]| Foreach-Object{ $PC.Monitor2 += $_;}}
-
-if([int]$Monitorsarray[33] -ne 0){
-$Monitorsarray[32..47]| Foreach-Object{ $PC.Monitor3 += $_;}}
-
-Write-Host "$amount"
-Write-Host "$Monitors"
-
-$currentmonitoramount = ($currentmonitorsntemp.SerialNumberID.count /16);
 
 
-ForEach($PC in $PClist){ #needs aproper sting here
+ForEach($PC in $PClist){ #needs aproper Import here
 Write-Host "$PC is the current Workstation";
 $PC = [pc]::new()
 $PC.Seriennummer = get-WmiObject Win32_BIOS | Select SerialNumber |%{$_.SerialNumber}
 $PC.PCName = $env:computername
 $PC.Username = $env:UserName
 
-$PC
-}
+{
+$Monitorsarray = Get-WmiObject -Property SerialNumberID -Namespace "root/WMI" WmiMonitorID |Select-Object  SerialNumberID | %{[char[]]($_.SerialNumberID)};
 
+if([int]$Monitorsarray[1] -ne 0){#Removes internal and Empty Serials
+$Monitorsarray[0..15]| Foreach-Object{ $PC.Monitor1 += $_;}}
+
+if([int]$Monitorsarray[17] -ne 0){#Removes internal and Empty Serials
+$Monitorsarray[16..31]| Foreach-Object{ $PC.Monitor2 += $_;}}
+
+if([int]$Monitorsarray[33] -ne 0){#Removes internal and Empty Serials
+$Monitorsarray[32..47]| Foreach-Object{ $PC.Monitor3 += $_;}}
+} #Monitor Detection
+
+{
+Get-ItemProperty -Path Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\usbccgp\Enum -name [1-9]| get-member -name "?"|
+    %{$_.Definition}|%{# The Prefix "string 1=" is handed over with the Same information that 
+        if(($_.substring(17,4) -eq "03F0") -and ($_.substring(26,4) -ne "2B4A")){#HP but not the Keyboard
+        $PC.USB += "HP:" + $_.substring(31)
+        }elseif(($_.substring(17,4) -eq "046D") -and ($_.substring(26,4) -ne "C31C")){#Logitec but not the Keyboard
+        $PC.USB += "Logitec:" + $_.substring(31)
+        }elseif(($_.substring(17,4) -eq "1B17") -and ($_.substring(26,4) -ne "C31C")){#Plusonic that is stealing the SHENZHEN e-loam VendorID
+        $PC.USB += "Plusonic:" + $_.substring(31)
+        }}} #USB Detection
+
+[void]$Ergebnis.add($PC)
+
+}
 
 $PC | Export-Csv -Path C:\Daten\TestOutput.csv -UseCulture -NoTypeInformation
 
 $Ergebnis | Export-Csv -Path -UseCulture -Encoding UTF8 -NoTypeInformation
 
-#Test:
+#Testing Stuff, dont carry to production:
 get-WmiObject Win32_BIOS | Select SerialNumber |%{$_.SerialNumber}
 get-WmiObject Win32_BIOS | Select SerialNumber |Get-ItemProperty -path "$_"
 
@@ -62,6 +80,3 @@ $temp = "Test" , "Killer" ,177013
 ForEach($current in $temp){
 Write-Host "Temp is $current"
 }
-
-Get-WmiObject -Property SerialNumberID -Namespace "root/WMI" WmiMonitorID |
-                Select-Object SerialNumberID |
