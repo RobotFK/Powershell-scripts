262db581620e280b71f3d37457f3d73c60617048
RobotFK <31999284+RobotFK@users.noreply.github.com>
2023-06-01 07:00:45 +0200

    Indented for readability

--- Fixmailv2.1.ps1
+++ Fixmailv2.1.ps1
@@ -1,26 +1,25 @@
-﻿#Dangerus if Used, but quite effective.
+#Dangerus if Used, but quite effective.
 #V2 now no longer destroys Good emails
 #V2.1 Added some description (namly this line that states that the code only is safe if we asume the scope of start and end to be limited to the foreach loop ;) )
 #By Fabian Keusen
 $Files = Get-ChildItem -Filter "*.eml"
 foreach ($File in $Files){
-$text =Get-Content -Path $File.Name;
-$start = $text| Where-Object { $_ -like "<table id=3D*" } 
-$end = $text| Where-Object { $_ -like "</table>"}
-if ($start) {
-$startIndex = [Array]::IndexOf($text, $start)
-}
-if ($end) {
-$endIndex = [Array]::IndexOf($text, $end)
-}
-if ($end -and $start){
-$newtext = $text[0..($startindex-1)] + $text[($endIndex+1)..($text.Count - 1)]
-Out-File -FilePath .\$path -InputObject $newtext
-Write-host "Edited" $path
-}else{
-Write-host "Ignored" $path
-}
+    $text =Get-Content -Path $File.Name;
+    $start = $text| Where-Object { $_ -like "<table id=3D*" } 
+    $end = $text| Where-Object { $_ -like "</table>"}
 
+    if ($start) {
+        $startIndex = [Array]::IndexOf($text, $start)
+    }
+    if ($end) {
+        $endIndex = [Array]::IndexOf($text, $end)
+    }
+    if ($end -and $start){
+        $newtext = $text[0..($startindex-1)] + $text[($endIndex+1)..($text.Count - 1)]
+        Out-File -FilePath .\$path -InputObject $newtext
+        Write-host "Edited" $path
+    }else{
+        Write-host "Ignored" $path
+    }
 }
-
 Read-Host -Prompt "Done, press enter to Close this window"
